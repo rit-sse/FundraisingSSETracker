e4d47e2a23f230f08a262efbef75f68ec829a982
Kocsen Chung <kocsenc@gmail.com>
2013-04-07 16:56:50 -0400

    CLI Revamp. Use cases, added documentation

--- FoodTracker.rb
+++ FoodTracker.rb
@@ -1,39 +1,58 @@
 require './FoodItem.rb'
 
+# The Food Tracker Class is the CLI interface 
+# which holds a Hash Table.
+# The hash table called @table is the data structure
+# to hold the FoodItem objec and a count of that FootItem
 class FoodTracker
 
   def initialize()
     @table = Hash.new()
+    sysout("Starting Food Tracker")
   end
 
   def command_line()
-    instructions = "(n)ew , (a)dd, (r)ead, (q)uit\nput in a hash to remove one element of it, or add a new item to store it in the database.\n"
-    puts(instructions)
-    input = gets.chomp
-    puts()
-    while(input != "q")
-      if input == "a"
+    prompt = "(n)ew , (a)dd, (r)ead, (q)uit\nput in a hash to remove one element of it, or add a new item to store it in the database.\n"
+    
+    while(true) do
+      puts()
+      sysout( prompt )
+      print ">> "
+
+      input = gets 
+      abort("EOF, terminating program...") if input == nil
+      input = input.chomp
+      input.downcase!
+
+      case input
+      when "a"
         add_item_cmd
-      elsif input == "r"
+
+      when "r"
         read_items
-      elsif input == "n"
+
+      when "n"
         new_item
+
+      when "q"
+        shutdown()
+        break
+
       else
         if @table.has_key?(input)
           @table[input].sub()
         else
           new_item(input)
         end
+
       end
-      puts()
-      puts(instructions)
-      input = gets.chomp
     end
   end
 
   def new_item(upc=gets.chomp)
     @table[upc] = FoodItem.new(upc, 0)
   end
+
 
   def add_item_cmd
     puts("item: ")
@@ -42,13 +61,32 @@ class FoodTracker
     number = gets.chomp.to_i
     add_item(@table[item],number)
   end
+
+  # Add Item
   def add_item(item, number)
     item.add(number)
   end
+
+  # Remove item
   def remove_item(item)
     item.sub()
   end
+
+  # Read items
   def read_items()
     @table.each {|key,value| puts value}
   end
+
+  # System IO for future debugging
+  def sysout(string)
+    puts ("[SYSTEM]: " + string)  
+  end
+
+  # Shutdown sequence when cli recieves 'q'
+  def shutdown()
+    sysout("System going for shutdown")
+    #Do I/O (save to file for example)
+    abort("Food Item Tracker Terminated")
+  end
+
 end
--- ProgramStart.rb
+++ ProgramStart.rb
@@ -1,3 +1,5 @@
+# Initiation file for the Food Inventory Tracker
+
 require './FoodTracker.rb'
 ft = FoodTracker.new()
 ft.command_line
