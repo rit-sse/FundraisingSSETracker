63c08e01aeeb4290bc676cd524e0b1d36709b40a
kristenmills <kristen@kristen-mills.com>
2013-05-05 18:52:23 -0400

    pagination

--- web/site.rb
+++ web/site.rb
@@ -1,4 +1,8 @@
 require 'sinatra/base'
+require 'will_paginate'
+require 'will_paginate/active_record'
+require 'will_paginate/view_helpers/sinatra'
+require 'will_paginate-bootstrap'
 require '../DatabaseModels'
 require './helper'
 
@@ -7,18 +11,20 @@ require './helper'
 class Site < Sinatra::Base
   use Rack::Deflater
   use ActiveRecord::ConnectionAdapters::ConnectionManagement
+
+  helpers WillPaginate::Sinatra::Helpers
 	#pages
 	get '/' do
 		erb :index
 	end
 
   get '/percent-purchased' do
-    @items = Item.all
+    @items = Item.paginate(page: params[:page], per_page: 8)
     erb :percent_purchased
   end
 
   get '/percent-purchased-data' do
-    @items = Item.all
+    @items = Item.paginate(page: params[:page], per_page: 8)
     @data_shown = params[:data_shown]
     erb :"percent-purchased-data", layout: false
   end
