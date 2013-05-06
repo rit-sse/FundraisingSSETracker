984a3ccf745de8f886a92d954e19dea1fec771d9
kristenmills <kristen@kristen-mills.com>
2013-05-06 17:23:32 -0400

    Validations and things

--- DatabaseModels.rb
+++ DatabaseModels.rb
@@ -5,20 +5,27 @@ class Item < ActiveRecord::Base
   has_many :scans
   has_one :inventory
 
+  validates :cost, :retail_price, numericality: true
+
   def to_s
     "#{name} (#{upc})"
   end
 end
 
 class Scan < ActiveRecord::Base
-  attr_accessible :time, :purchase, :quantity, :item_id
+  attr_accessible :time, :purchase, :quantity
+  belongs_to :item
 
+  validates :quantity, numericality: {only_integer: true}
   def to_s
    "#{time}\t#{quantity} items (#{purchase ? "sale" : "inventory stock"})"
   end
 end
 
 class Inventory < ActiveRecord::Base
-  attr_accessible :amount, :sold, :item_id
+  attr_accessible :amount, :sold
+  belongs_to :item
+
+  validates :amount, :sold, numericality: {only_integer: true}
 end
 
